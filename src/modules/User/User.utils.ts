d8eb87a84aa12ee30b68999244ff85ad4e446cc9
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-09-14 00:50:47 +0530

    bug fix

--- src/modules/User/User.utils.ts
+++ src/modules/User/User.utils.ts
@@ -16,11 +16,28 @@ const findLastStudentId = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4); //2030
+
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentSemesterYear === currentSemesterYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let increment = (Number(currentId) + 1).toString().padStart(4, '0');
 
   increment = `${payload.year}${payload.code}${increment}`;
